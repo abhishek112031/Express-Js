bc927ac1b468ba10576942ca3035e6f3d80395fa
abhishek112031 <105448345+abhishek112031@users.noreply.github.com>
2022-11-30 12:17:50 +0530

    Adding body-parser-02
    console log  the input post  data from client side..

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const http=require('http'); dont need while using express frameworks.
 const express=require('express');
+const bodyParser=require('body-parser');
 
 //express application storing in a constant :
 const app=express();
@@ -9,14 +10,21 @@ const app=express();
 //it acceps array of requestHandellers:
 //the anonymous /call back function inside it will be executed for every incomming requests.
 //next is a function that we pass inside cb function:
-app.use((req,res,next)=>{
-    console.log("in the middleware!");
-    next();//to allow request to continue to next middleware inline:
-           //we should call next() if we dont want to send response otherwise req wiill die.. 
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/app-product',(req,res,next)=>{
+   
+    res.send( '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');//send allows us to attach a body,which is a type of any..
 });
 
-app.use((req,res,next)=>{
-    console.log("in the next middleware!");
+
+//app.get() :for incomming get request, app.post() :for in comming post request
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    
     res.send( '<h1>hi from express</h1>');//send allows us to attach a body,which is a type of any..
 });
 
